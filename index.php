a3f08d41180fd40e4876d7fdf296aead49ab7363
datnguyendesign <ngtiendat.94.04@gmail.com>
2023-11-04 09:58:40 +0700

    1st

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Platform - Homepage</title>
+
+    <script src="https://kit.fontawesome.com/4f40e8708a.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <?php include_once'sidebar.php' ?>
+</body>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sidebar.css">
+    <script src="https://kit.fontawesome.com/4f40e8708a.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div id="sidebar">
+        <a href="#" class="logo">
+            <img src="images/logo.svg">
+            <h1>WaveM</h1>
+        </a>
+        <ul class="links">
+            <li class="link">
+                <i class="fa fa-house"></i>
+                <p>Home</p>
+            </li>
+            <li class="link">
+                <i class="fa fa-book"></i>
+                <p>Playlist</p>
+            </li>
+            <li class="link">
+                <i class="fa fa-podcast"></i>
+                <p>Podcast</p>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
